94702380fed655aa95cad80f671b092d7ee26046
kumar76sumit <142105432+kumar76sumit@users.noreply.github.com>
2024-02-18 22:20:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+	let inputs=document.querySelectorAll("input");
+	let firstName=inputs[0].value;
+	let lastName=inputs[1].value;
+	let button=inputs[2].value;
+	button.addEventListener("click",alert(firstName + " " + lastName));
 }
