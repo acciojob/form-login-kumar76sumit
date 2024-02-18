e9d38f6d13f299ed9668f340c8497520b3649a8d
kumar76sumit <142105432+kumar76sumit@users.noreply.github.com>
2024-02-18 22:37:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function getFormvalue() {
 	let inputs=document.querySelectorAll("input");
 	let firstName=inputs[0].value
 	let lastName=inputs[1].value
-	let fullName=firstName+" "+lastName
+	let fullName= firstName+" "+lastName
 	alert(fullName)
 }
